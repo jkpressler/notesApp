8599442bd3a50e76bba53bb77a45082aa4dd4969
Josh P <joshpressler96@gmail.com>
2021-02-05 14:56:36 +0900

    Commented out recyclerview parts, added a non-functioning search bar

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -61,7 +62,6 @@ class MainActivity : AppCompatActivity(), Observer{
             addNote()                                                           //go to add a note page when "New Note" button is clicked
         }
     }
-
 
     private fun addNote(){
         val intent = Intent(this, AddNoteActivity::class.java)
